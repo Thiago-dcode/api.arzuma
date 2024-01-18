85ca7f280193273ed360c1b170e53939cd466f5a
Thiago-dcode <contact.thiago.ferreira@gmail.com>
2024-01-18 09:32:03 +0100

    connection with pyme in rojas

--- app/Http/Controllers/Intranet/Modules/ImportArticulosController.php
+++ app/Http/Controllers/Intranet/Modules/ImportArticulosController.php
@@ -8,12 +8,10 @@ use App\Rules\TxtFileRule;
 use App\Intranet\Utils\Validate;
 use App\Traits\HttpResponses;
 use App\Http\Controllers\Controller;
-use App\Intranet\Utils\Path;
 use Illuminate\Support\Facades\Storage;
 use App\Intranet\Modules\ImportArt;
-use App\Intranet\Utils\Constants;
 use Illuminate\Support\Facades\Log;
-use PHPUnit\TextUI\Configuration\Constant;
+
 
 class ImportArticulosController extends Controller
 {
@@ -29,7 +27,7 @@ class ImportArticulosController extends Controller
 
 			return response("You are not authorized to access to importArticulos module", 401);
 		}
-	
+
 		try {
 
 			$request->validate([
@@ -38,79 +36,115 @@ class ImportArticulosController extends Controller
 			$uploadedFile = $request->file('file');
 
 			$file = $uploadedFile->getClientOriginalName();
-		
+
 			$today = date('d-m-Y');
 			$moduleName = $request->user()["module_active"];
-			
+
 			$desirePath = "uploads/$companyName/$today/";
 			$path = $uploadedFile->storeAs($desirePath, $file);
 			$fileContent = Storage::get($path);
-		
+
 			$articulos = ImportArt::getArticulosFromTxt($fileContent);
-			
+
 			if (!$articulos) {
 
 				return $this->error(['errors' => [
-					'file' => 'No article found inside the ' . $file
-				]], "Validation error", 401);
+					'file' => 'No se ha encontrado artículos dentro del archivo ' . $file
+				]], "Validation error", 400);
 			}
-			
+
 			config(['logging.channels.modules.path' => storage_path("logs/modules/$moduleName/" . $today . '.log')]);
+
+			$responses = [
+				'total' => count($articulos),
+				'error' => 0,
+				'update' => 0,
+				'insert' => 0,
+				'messages' => []
+			];
 			
-			//check if exist articulo
 			foreach ($articulos as $codigo => $articulo) {
-				
-				$pymeArticulo = ImportArt::getArticulo($companyName, $codigo);
-				dd($pymeArticulo);
-				if ($pymeArticulo) {
-					//log debug if exist or not
+				 $result = ImportArt::getArticulo($companyName, $codigo);
+					$result = [
+					'error' => 'something went wrong'
+				];
+				if (isset($result['error'])) {
+
+					Log::channel('modules')->error("ERROR en SELECT de Articulo con codigo: $codigo, {$result['error']}");
+					array_push($responses['messages'], [
+						'type' => 'error',
+						'message' => "ERROR en SELECT de Articulo con codigo: $codigo, {$result['error']}"
+					]);
+					$responses['error']++;
+					continue;
+				}
+				if ($result['status']) {
+
 
 					Log::channel('modules')->debug("Articulo con codigo: $codigo existe en PYME empezando UPDATE");
-					// update the precio,cantidad, codbar, iva
-					$update = true;
+
+					$update = ImportArt::update($codigo,$articulo);
 
 					if ($update['status']) {
-						Log::channel('modules')->info("UPDATE de Articulo con codigo: $codigo, precio: {$pymeArticulo['precio']}->{$articulo['precio']} cantidad: {$pymeArticulo['cantidad']}->{$articulo['cantidad']} iva: {$pymeArticulo['iva']}->{$articulo['iva']} codbar: {$pymeArticulo['codbar']}->{$articulo['codbar']}");
+
+						// cantidad: {$result['data']['cantidad']}->{$articulo['cantidad']} iva: {$result['data']['iva']}->{$articulo['iva']} codbar: {$result['data']['codbar']}->{$articulo['codbar']}
+					
+						array_push($responses['messages'], [
+							"type" => 'update',
+							"message" => "UPDATE de Articulo con codigo: $codigo, precio: {$result['data']['PRECIO']} --> {$articulo['precio']}"
+
+						]);
+						$responses['update']++;
+						Log::channel('modules')->info("UPDATE de Articulo con codigo: $codigo, precio: {$result['data']['PRECIO']} --> {$articulo['precio']}");
 						continue;
 					}
-
+					array_push($responses['messages'], [
+						"type" => 'error',
+						'message' => "ERROR en UPDATE de Articulo con codigo: $codigo, {$update['error']}"
+					]);
+					$responses['error']++;
 					Log::channel('modules')->error("ERROR en UPDATE de Articulo con codigo: $codigo, {$update['error']}");
-					//log info with the previus articulo data and the new one
+
 
 					continue;
 				}
-				Log::channel('modules')->debug("Articulo con codigo: $codigo  NO existe en PYME empezando INSERT");
-				//insert with codigo, nombre, precio, cantidad, codbar,iva
-				//log info of the insert
 
-				$insert = true;
+				Log::channel('modules')->debug("Articulo con codigo: $codigo  NO existe en PYME empezando INSERT");
+
+				$insert = [
+					'status' => false,
+					'error' => 'Error in insert'
+				];
 
 				if ($insert['status']) {
+
+					array_push($responses['messages'], [
+						'type' => 'insert',
+						'message' => "INSERT de Articulo con codigo: $codigo, nombre: {$articulo['nombre']}  precio: {$articulo['precio']} cantidad: {$articulo['cantidad']} iva: {$articulo['iva']} codbar: {$articulo['codbar']}"
+					]);
+					$responses['insert']++;
 					Log::channel('modules')->info("INSERT de Articulo con codigo: $codigo, nombre: {$articulo['nombre']}  precio: {$articulo['precio']} cantidad: {$articulo['cantidad']} iva: {$articulo['iva']} codbar: {$articulo['codbar']}");
 					continue;
 				}
-
-				Log::channel('modules')->error("ERROR en INSERT de Articulo con codigo: $codigo, {$update['error']}");
+				array_push($responses['messages'], [
+					"type" => "error",
+					'message' => "ERROR en INSERT de Articulo con codigo: $codigo, {$insert['error']}"
+				]);
+				$responses['error']++;
+				Log::channel('modules')->error("ERROR en INSERT de Articulo con codigo: $codigo, {$insert['error']}");
 				//log info with the previus articulo data and the new one
 
 				continue;
 			}
-
-
-
-
-
-
-
-			return $this->success(['path' => $path], 'Success');
+			return $this->success($responses, 'Success');
 			//code...
 		} catch (ValidationException $e) {
 			// Handle validation errors
 
-			return $this->error(['errors' => $e->errors()], "Validation error", 401);
+			return $this->error(['errors' => $e->errors()], "Validation error", 400);
 		} catch (\Exception $e) {
 			// Handle other exceptions (e.g., database errors)
-			return $this->error([], $e->errors(), 401);
+			return $this->error(['errors' => []], $e->getMessage(), 500);
 		}
 	}
 }
--- app/Intranet/Modules/ImportArt.php
+++ app/Intranet/Modules/ImportArt.php
@@ -8,36 +8,289 @@ use App\Intranet\Utils\Path;
 use App\Intranet\Utils\Utils;
 use App\Intranet\Utils\Constants;
 use App\Intranet\Pyme\PymeConnection;
+use PDOException;
 use Symfony\Component\HttpFoundation\Request;
 
 
 class ImportArt
 {
     static $firebird = null;
-    private static function connect($company)
+    private static function connect($company = 'bera-textil')
     {
-
+        //'185.226.177.3:3055:C:\Distrito\PYME\DATABASE\ROJASDIS\2023.FDB'
         try {
-            static::$firebird = PymeConnection::start(Constants::get($company));
+            static::$firebird = PymeConnection::start(Constants::get('bera-textil'));
         } catch (\Throwable $th) {
-            
-            echo $th->getMessage();
-        }
 
-      
+            throw new PDOException("Pyme ($company) connection error: " . $th->getMessage(), 1);
+        }
     }
 
     public static function getArticulo($company, $codigo)
     {
 
-        if (!static::$firebird) self::connect($company);
-        $sql = "select * from articulo where codigo=:codigo";
+        try {
+            if (!static::$firebird) self::connect($company);
+            $sql = "select CODIGO as codigo, NOMBRE as nombre, PRECIOCOSTE as precio from articulo where codigo=:codigo";
+
+            $stmt = static::$firebird->prepare($sql);
+            $stmt->execute([
+                'codigo' => 'P1010764'
+            ]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($result) {
+                return [
+                    'status' => true,
+                    'data' => $result,
+                ];
+            }
+            return [
+                'status' => false,
+                'data' => [],
+            ];
+        } catch (\Throwable $th) {
+            return [
+                'status' => false,
+                'data' => [],
+                'error' => $th->getMessage()
+            ];
+        }
+    }
+    public static function getTipoIva($iva)
+    {
+        try {
+            if (!static::$firebird) self::connect();
+            $sql = "select * from empresa";
+            $stmt = static::$firebird->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            foreach ($result as $key => $value) {
+                $key = strtolower($key);
+                $ivaStr = substr($key, 0, strlen($key) - 1);
+                if ($ivaStr  !== 'iva') continue;
+                if ((int)$value !== (int)$iva) continue;
+                $tipoIva = (int) str_replace($ivaStr, "", $key);
+                if (!is_int($tipoIva)) continue;
+                return $tipoIva;
+            }
+            return 0;
+        } catch (\Throwable $th) {
+            throw new PDOException($th->getMessage());
+        }
+    }
+    public static function update($codigo, $articulo)
+    {
+
+        try {
+            $result1 = self::updateArticulo($codigo, $articulo);
+            $result2 = self::updateCodBar($codigo, $articulo);
+            $result3 = self::updateCompra($codigo, $articulo);
+            // $result4 = self::updateCantidad($codigo, $articulo);
+            if ($result1 && $result2  && $result3) {
+                return [
+                    'status' => true,
+                    'data' => [],
+                ];
+            }
+            return [
+                'status' => false,
+                'data' => [],
+                'error' => 'Hubo errores insertando'
+            ];
+        } catch (\Throwable $th) {
+            return [
+                'status' => false,
+                'data' => [],
+                'error' => $th->getMessage()
+            ];
+        }
+    }
+    public static function insert($codigo, $articulo)
+    {
+        try {
+            $result1 = self::insertArticulo($codigo, $articulo);
+            $result2 = self::insertCodbar($codigo, $articulo);
+            $result3 = self::insertCompra($codigo, $articulo);
+
+            if ($result1 && $result2  && $result3) {
+                return [
+                    'status' => true,
+                    'data' => [],
+                ];
+            }
+            return [
+                'status' => false,
+                'data' => [],
+                'error' => 'No se ha podido insertar'
+            ];
+        } catch (\Throwable $th) {
+            return [
+                'status' => false,
+                'data' => [],
+                'error' => $th->getMessage()
+            ];
+        }
+    }
+
+    private static function updateArticulo($codigo, $articulo)
+    {
+        $tipoiva = self::getTipoIva($articulo['iva']);
+        $sql = "UPDATE articulo SET PRECIOCOSTE=:precio, tipoiva=:tipoiva  where codigo=:codigo";
 
         $stmt = static::$firebird->prepare($sql);
-        $stmt->execute([
+        $result = $stmt->execute([
+            'precio' => (float)$articulo['precio'],
+            'tipoiva' => $tipoiva,
             'codigo' => $codigo
         ]);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    private static function insertArticulo($codigo, $articulo)
+    {
+        $vars = [
+            'codigo' => (string) $codigo,
+            'codmarca' => null,
+            'nombre' => (string) $articulo['nombre'],
+            'descripcion' => (string) utf8_decode($articulo['nombre'] . '<br><br>'  . ',' . '' . ',' . ''),
+            'preciocoste' => (float) $articulo['precio'],
+            'precioventa' => null,
+            'codfamilia' => null,
+            'baja' => 'F',
+            'tipoactualizacion' => 0,
+            'tipoiva' => $articulo['iva'],
+            'tipoivareducido' => 1,
+            'tipoivacompra' => 0,
+            'tipoivacomprareducido' => 1,
+            'controlstock' => 1,
+            'unidaddecimales' => 0,
+            'preciodecimales' => 2,
+            'costedecimales' => 2,
+            'stockfactor' => 1,
+            'etiquetasegununidadmedida' => 0,
+            'proveeddefecto' => null,
+            'ubicacion' => 0,
+            'descripcioncorta' => $articulo['nombre'],
+            'formatodesccorta' => 0,
+            'formatodescripcion' => 2,
+            'metakeywords' => null,
+            'aplicarinvsujetopasivo' => 0,
+            'tipobc3' => 20,
+            'unidadcontrolcarubicstock' => 0,
+            'excluirweb' => 'T',
+        ];
+        $sql = 'INSERT INTO articulo 
+        (codigo, codmarca, nombre, codfamilia, baja, descripcion, preciocoste, precioventa, 
+        tipoactualizacion, tipoiva, tipoivareducido, tipoivacompra, tipoivacomprareducido, controlstock, 
+        unidaddecimales, preciodecimales, costedecimales, stockfactor, etiquetasegununidadmedida, proveeddefecto, 
+        ubicacion, descripcioncorta, formatodesccorta, formatodescripcion, metakeywords, aplicarinvsujetopasivo, 
+        tipobc3, unidadcontrolcarubicstock, excluirweb) 
+        VALUES 
+        (:codigo, :codmarca, :nombre, :codfamilia, :baja, :descripcion, :preciocoste, :precioventa, 
+        :tipoactualizacion, :tipoiva, :tipoivareducido, :tipoivacompra, :tipoivacomprareducido, :controlstock, 
+        :unidaddecimales, :preciodecimales, :costedecimales, :stockfactor, :etiquetasegununidadmedida, :proveeddefecto, 
+        :ubicacion, :descripcioncorta, :formatodesccorta, :formatodescripcion, :metakeywords, :aplicarinvsujetopasivo, 
+        :tipobc3, :unidadcontrolcarubicstock, :excluirweb)';
+
+        $stmt = $firebird->prepare($sql);
+
+        // Execute the query with parameters
+        return $stmt->execute($vars);
+    }
+    private static function updateCodBar($codigo, $articulo)
+    {
+       
+        $sql = 'UPDATE codbarra 
+        SET codbarras = :codbarras
+        WHERE codarticulo = :codarticulo
+        AND codcaract = :codcaract
+        AND valorcaract = :valorcaract';
+
+        $stmt = $pdo->prepare($sql);
+
+        // Execute the query with parameters
+        $stmt->execute([
+            ':codbarras' => $articulo['codbar'],
+            ':codarticulo' => $codigo,
+            ':codcaract' => null,
+            ':valorcaract' => null,
+        ]);
+
+        return $result;
+    }
+    private static function insertCodbar($codigo, $articulo)
+    {
+
+        $sql = "INSERT INTO codbarra
+        (codarticulo, codcaract, valorcaract, codbarras)
+        VALUES
+        (:codarticulo, :codcaract, :valorcaract, :codbarras)";
+
+        $stmt = $firebird->prepare($sql);
+
+        // Execute the query with parameters
+        $result = $stmt->execute([
+            ':codbarras' => $articulo['codbar'],
+            ':codarticulo' => $codigo,
+            ':codcaract' => null,
+            ':valorcaract' => null,
+        ]);
+
+        return $result;
+    }
+    private static function updateCompra($codigo, $articulo)
+    {
+
+        $sql = 'UPDATE compra 
+                SET preciocoste = :preciocoste
+                WHERE codarticulo = :codarticulo
+                AND codcaract = :codcaract
+                AND valorcaract = :valorcaract';
+
+        $stmt = $firebird->prepare($sql);
+
+
+        $result = $stmt->execute([
+            ':preciocoste' => (float)$articulo['precio'],
+            ':codarticulo' => $codigo,
+            ':codcaract' => null,
+            ':valorcaract' => null,
+        ]);
+
+        return $result;
+    }
+
+
+    private static function insertCompra($codigo, $articulo)
+    {
+
+        $sql = 'INSERT INTO compra 
+        (codarticulo, codproveedor, preciocoste, preciotarifa, descuento) 
+        VALUES 
+        (:codarticulo, :codproveedor, :preciocoste)';
+
+        $stmt = $firebird->prepare($sql);
+
+        // Execute the query with parameters
+        $result = $stmt->execute([
+            ':preciocoste' => (float)$articulo['precio'],
+            ':codarticulo' => $codigo,
+            ':codproveedor' => null
+        ]);
+        return $result;
+    }
+    private static function updateCantidad($codigo, $articulo)
+    {
+        $sql = "UPDATE EXISTENC SET STOCK1=:cantidad where codigo=:codigo";
+
+        $stmt = static::$firebird->prepare($sql);
+        $result = $stmt->execute([
+            'cantidad' => $articulo['cantidad'],
+            'codigo' => $codigo
+        ]);
+
         return $result;
     }
 
